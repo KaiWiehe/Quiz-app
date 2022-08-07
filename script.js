dae2cee13846504560c71d4d2c5183ff9377aa37
KaiWiehe <Kai_Wiehe@live.de>
2022-08-07 12:23:40 +0200

    added sounds and cleaned up the code

--- script.js
+++ script.js
@@ -81,98 +81,53 @@ let questions = [{
 ];
 
 let score = 0; /* der punktestand startet bei 0 und wird bei jeder richtigen antwort um 1 erhöht */
-
 let currentQuestion = 0; /* Der anfangswert, damit er bei dem ersten json array inhalt anfängt */
+let audio_good = new Audio('good.mp3'); /* audio "Richtige Frage" */
+let audio_wrong = new Audio('wrong.mp3'); /* audio "Falsche Frage" */
 
 function init() {
     let question_lenght = document.getElementById('question_lenght');
-    question_lenght.innerHTML = questions.length;
-
-    /* document.getElementById('end-screen-button').classList.add('hide'); */
+    question_lenght.innerHTML = questions.length; /* Zeigt an wie viele Fragen es gibt */
 
     showQuestion();
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
-
-    let question_titel = document.getElementById('question_titel'); /* Für die bessere lesbarkeit */
-    let answer_1 = document.getElementById('answer_1');
-    let answer_2 = document.getElementById('answer_2');
-    let answer_3 = document.getElementById('answer_3');
-    let answer_4 = document.getElementById('answer_4');
-
-    if (currentQuestion >= questions.length) {
-        /* Show endscreen */
-        question_titel.innerHTML = 'Ende!'
-        answer_1.innerHTML = '';
-        answer_2.innerHTML = 'Du hast es bis zum Ende geschafft';
-        answer_3.innerHTML = `Du hast <b>${score}</b> von <b>${questions.length}</b> Fragen richtig beantwortet!`; /* TODO punktestand einfügen */
-        answer_4.innerHTML = '';
-
-        removeAllAnswerClasses();
-
-        document.getElementById('next-button').parentNode.style.display = 'none'; /* löscht den ganzen unteren bereich wo der Button ist */
-
-        document.getElementById('questionFooter2').style = ''; /* fügt den button "neues spiel" ein */
-
-        document.getElementById('img').src = "medal.png"; /* ändert das bild im endscreen */
-
-        document.getElementById('progress-bar').parentNode.classList.add("hide"); /* lässt die progressbar verschwinden */
-    } else { /* Show next screen */
-        document.getElementById('progress-bar').innerHTML = `${calcPercent()}%`;
-        document.getElementById('progress-bar').style.width = `${calcPercent()}%`;
-
-        question_titel.innerHTML = question["question"]; /* fügt die aktuelle frage ein */
-        answer_1.innerHTML = question["answer_1"];
-        answer_2.innerHTML = question["answer_2"];
-        answer_3.innerHTML = question["answer_3"];
-        answer_4.innerHTML = question["answer_4"];
+    if (gameIsOver()) {
+        showEndScreen();
+    } else {
+        updateProgressBar();
+        showNextQuestion();
     }
 }
 
 function answer(answer) {
-    let question = questions[currentQuestion]; /* Damit die nachfolgende schreibweise leichter wird */
-    let answerNumber = answer.slice(-1); /* Damit nimmt er die letzte zahl, also aus 'answer_3' wird '3' */
-    let idOfRightAnswer = `answer_${question["right_answer"]}`; /* Könnte ich auch so reinschreiben, sieht aber so schöner aus */
-
-    if (answerNumber == question["right_answer"]) { /* Wenn ich die richtige antwort gedrückt habe passiert das... */
-        document.getElementById(answer).parentNode.classList.add('bg-success'); /* Mit parentNode geht er auf das übergeordnete Element */
-        score++;
+    if (pressRightAnswer(answer)) {
+        updateColorAndScore(answer);
+        audio_good.play();
     } else { /* Wenn die antwort falsch war passiert das... */
-        document.getElementById(answer).parentNode.classList.add('bg-danger');
-        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        showBothColors(answer);
+        audio_wrong.play();
     }
-    document.getElementById('next-button').disabled = false; /* damit wird der Button enabled */
+    enableButton();
     removeOnClickAndHover();
 }
 
 function nextQuestion() {
     currentQuestion++; /* von 0 auf 1 usw... */
     resetColors();
-    document.getElementById('next-button').disabled = true; /* damit wird der Button wieder disabled */
-
-    let current_question = document.getElementById('current-question');
-    current_question.innerHTML = currentQuestion + 1; /* Damit zeigt er die Aktuelle frage an */
-
+    disableButton();
+    updateCurrentQuestion();
     showQuestion(); /* Um die neue Frage zu laden */
 
-    if (currentQuestion < questions.length) {
+    if (notTheEnd()) {
         addOnClickAndHover();
     }
 }
 
 function nextGame() {
-    currentQuestion = 0; /* setzt den wert wieder auf den anfangswert */
-    score = 0; /* setzt den wert wieder auf den anfangswert */
-    document.getElementById('questionFooter2').style.display = 'none'; /* der end screen button verschwindet wieder */
-    document.getElementById('img').src = "quiz.jpg"; /* ändert das bild wieder zurück */
-    document.getElementById('progress-bar').parentNode.classList.remove("hide"); /* zeigt die progress bar wieder an */
-    document.getElementById('next-button').parentNode.style = ''; /* Zeigt den unteren bereich wieder an */
-    addAllAnswerClasses();
-    let current_question = document.getElementById('current-question');
-    current_question.innerHTML = currentQuestion + 1; /* Damit zeigt er die Aktuelle frage an */
-    showQuestion(); /*  */
+    resetAll();
+    showQuestion(); /* Zeigt die erste frage wieder an */
 }
 
 
@@ -219,4 +174,94 @@ function addOnClickAndHover() { /* TODO funktuniert noch nicht, er fügt das onc
         document.getElementById(`answer_${i}`).parentNode.setAttribute('onclick', `answer('answer_${i}')`);
         document.getElementById(`answer_${i}`).parentNode.classList.add('quiz-answer-card');
     }
+}
+
+function showEndScreen() {
+    /* Show endscreen */
+    question_titel.innerHTML = 'Ende!'
+    answer_1.innerHTML = '';
+    answer_2.innerHTML = 'Du hast es bis zum Ende geschafft!';
+    answer_3.innerHTML = `Du hast <b>${score}</b> von <b>${questions.length}</b> Fragen richtig beantwortet!`;
+    answer_4.innerHTML = '';
+
+    removeAllAnswerClasses();
+    document.getElementById('next-button').parentNode.style.display = 'none'; /* löscht den ganzen unteren bereich wo der Button ist */
+    document.getElementById('questionFooter2').style = ''; /* fügt den button "neues spiel" ein */
+    document.getElementById('img').src = "medal.png"; /* ändert das bild im endscreen */
+    document.getElementById('progress-bar').parentNode.classList.add("hide"); /* lässt die progressbar verschwinden */
+}
+
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
+function updateProgressBar() {
+    document.getElementById('progress-bar').innerHTML = `${calcPercent()}%`;
+    document.getElementById('progress-bar').style.width = `${calcPercent()}%`;
+}
+
+function showNextQuestion() {
+    let question = questions[currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
+
+    let question_titel = document.getElementById('question_titel'); /* Für die bessere lesbarkeit der nachfolgenden */
+    let answer_1 = document.getElementById('answer_1');
+    let answer_2 = document.getElementById('answer_2');
+    let answer_3 = document.getElementById('answer_3');
+    let answer_4 = document.getElementById('answer_4');
+
+    question_titel.innerHTML = question["question"]; /* fügt die aktuelle frage ein */
+    answer_1.innerHTML = question["answer_1"];
+    answer_2.innerHTML = question["answer_2"];
+    answer_3.innerHTML = question["answer_3"];
+    answer_4.innerHTML = question["answer_4"];
+}
+
+function pressRightAnswer(answer) {
+    let question = questions[currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
+    let answerNumber = answer.slice(-1); /* Damit nimmt er die letzte zahl, also aus 'answer_3' wird '3' */
+
+    return answerNumber == question["right_answer"];
+}
+
+function updateColorAndScore(answer) {
+    document.getElementById(answer).parentNode.classList.add('bg-success'); /* Mit parentNode geht er auf das übergeordnete Element */
+    score++;
+}
+
+function showBothColors(answer) {
+    let question = questions[currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
+    let idOfRightAnswer = `answer_${question["right_answer"]}`; /* Könnte ich auch so reinschreiben, sieht aber so schöner aus */
+
+    document.getElementById(answer).parentNode.classList.add('bg-danger');
+    document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+}
+
+function enableButton() {
+    document.getElementById('next-button').disabled = false; /* damit wird der Button enabled */
+}
+
+function disableButton() {
+    document.getElementById('next-button').disabled = true; /* damit wird der Button wieder disabled */
+}
+
+function updateCurrentQuestion() {
+    let current_question = document.getElementById('current-question');
+    current_question.innerHTML = currentQuestion + 1; /* Damit zeigt er die Aktuelle frage an */
+}
+
+function notTheEnd() {
+    return currentQuestion < questions.length;
+}
+
+function resetAll() {
+    currentQuestion = 0; /* setzt den wert wieder auf den anfangswert */
+    score = 0; /* setzt den wert wieder auf den anfangswert */
+    document.getElementById('questionFooter2').style.display = 'none'; /* der end screen button verschwindet wieder */
+    document.getElementById('img').src = "quiz.jpg"; /* ändert das bild wieder zurück */
+    document.getElementById('progress-bar').parentNode.classList.remove("hide"); /* zeigt die progress bar wieder an */
+    document.getElementById('next-button').parentNode.style = ''; /* Zeigt den unteren bereich wieder an */
+    addAllAnswerClasses();
+    let current_question = document.getElementById('current-question');
+    current_question.innerHTML = currentQuestion + 1; /* Damit zeigt er die Aktuelle frage an */
+    addOnClickAndHover();
 }
