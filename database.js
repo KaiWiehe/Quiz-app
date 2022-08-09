ad780e55450ac2be2f44b1e4004afb5791118b60
KaiWiehe <Kai_Wiehe@live.de>
2022-08-09 14:14:23 +0200

    add categories

---
+++ database.js
@@ -0,0 +1,303 @@
+let questions = [{
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Leonard und Penny küssen sich nach ihrem ersten Date im Flur: Was bemerkt Leonard an der Decke?",
+        "answer_1": "Eine Überwachungskamera",
+        "answer_2": "Sheldon hat eine Notiz aufgehängt",
+        "answer_3": "Es hängt ein Mistelzweig über der Tür und deswegen küssen sie sich",
+        "answer_4": "Die Lampe ist defekt",
+        "right_answer": 1
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Wie oft klopft Sheldon an Pennys Tür?",
+        "answer_1": "Drei mal",
+        "answer_2": "Sechs mal",
+        "answer_3": "Neun mal",
+        "answer_4": "Zwölf mal",
+        "right_answer": 3
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Wo hat Howard vor der Beziehung mit Bernadette gewohnt?",
+        "answer_1": "Auf der Straße",
+        "answer_2": "Bei seiner Mutter",
+        "answer_3": "Bei seinem besten Freund Raj",
+        "answer_4": "In seiner eigenen Wohnung",
+        "right_answer": 2
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Wer heiratet zuerst?",
+        "answer_1": "Leonard & Penny",
+        "answer_2": "Raj & Bernadette",
+        "answer_3": "Sheldon & Amy",
+        "answer_4": "Howard & Bernadette",
+        "right_answer": 4
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Was ist Raj's Problem mit Frauen?",
+        "answer_1": "Er steht nicht auf Frauen",
+        "answer_2": "Er kann nicht reden wenn eine Frau im Raum ist, außer unter Alkoholeinfluß",
+        "answer_3": "Er kriegt eine schwache Blase wenn er mit einer Frau redet",
+        "answer_4": "Er lernt nie eine Frau kennen",
+        "right_answer": 2
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Welches Lied singt Penny Sheldon vor wenn er krank ist?",
+        "answer_1": "Katzentanzlied",
+        "answer_2": "Kommt ein Vogel geflogen",
+        "answer_3": "Hänsel und Gretel",
+        "answer_4": "Old Macdonald Had a Farm",
+        "right_answer": 1
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Wo im Appartement von Leonard und Sheldon befindet sich eine Kamera?",
+        "answer_1": "In der Deckenlampe",
+        "answer_2": "In der Aquaman Figur",
+        "answer_3": "In der Küche",
+        "answer_4": "Hinter einem Buch",
+        "right_answer": 2
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "In welcher Staffel kommt Amy dazu?",
+        "answer_1": "In der zweiten Staffel",
+        "answer_2": "In der dritten Staffel",
+        "answer_3": "In der vierten Staffel",
+        "answer_4": "In der fünften Staffel",
+        "right_answer": 2
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Wie nennen die anderen Sheldon und Amy auch?",
+        "answer_1": "Die Besserwisser",
+        "answer_2": "Freaks",
+        "answer_3": "Es gibt keinen Spitznamen",
+        "answer_4": "Shamy",
+        "right_answer": 4
+    },
+    {
+        'category': 'bigBangTheory',
+        'background': 'img/physics.png',
+        "question": "Hat Sheldon Geschwister?",
+        "answer_1": "Er hat keine Geschwister",
+        "answer_2": "Einen Zwillingsbruder",
+        "answer_3": "Eine Schwester",
+        "answer_4": "Eine Zwillingsschwester und einen Bruder",
+        "right_answer": 4
+    },
+    /* Rügen */
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Rügen ist...",
+        "answer_1": "die größte Insel Deutschlands",
+        "answer_2": "die zweitgrößte Insel Deutschlands",
+        "answer_3": "keine Insel",
+        "answer_4": "eine Halbinsel",
+        "right_answer": 1
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Auf Rügen fährt die Dampflokomotive?",
+        "answer_1": "Der Rasende Ronny",
+        "answer_2": "Der Rollende Roland",
+        "answer_3": "Der Rasende Roland",
+        "answer_4": "Der Rollende Ronny",
+        "right_answer": 3
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Wie groß ist Rügen?",
+        "answer_1": "ca. 724 km²",
+        "answer_2": "ca. 926 km²",
+        "answer_3": "ca. 1115 km²",
+        "answer_4": "ca. 873 km²",
+        "right_answer": 2
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Wie viele Brückenverbindungen zwischen dem Festland und der Insel gibt es?",
+        "answer_1": "Eine",
+        "answer_2": "Zwei",
+        "answer_3": "Drei",
+        "answer_4": "Vier",
+        "right_answer": 2
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Wie lang ist die neue Rügenbrücke?",
+        "answer_1": "1.375 Meter",
+        "answer_2": "1.897 Meter",
+        "answer_3": "2.543 Meter",
+        "answer_4": "2.831 Meter",
+        "right_answer": 4
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Was ist das 'Tor' zur Insel Rügen?",
+        "answer_1": "Greifswald",
+        "answer_2": "Binz",
+        "answer_3": "Stralsund",
+        "answer_4": "Sellin",
+        "right_answer": 3
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Welches Gewässer umgibt Rügen?",
+        "answer_1": "Nordsee",
+        "answer_2": "Ostsee",
+        "answer_3": "Atlantischer Ozean",
+        "answer_4": "Keiner von den genanten",
+        "right_answer": 2
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Welche Stadt gilt als Hauptstadt/Hauptort von der Insel Rügen?",
+        "answer_1": "Bergen auf Rügen",
+        "answer_2": "Sellin",
+        "answer_3": "Baabe",
+        "answer_4": "Sassnitz",
+        "right_answer": 1
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Was ist eine Sehenswürdigkeit auf Rügen?",
+        "answer_1": "Ozeaneum",
+        "answer_2": "Seebrücke Heringsdorf",
+        "answer_3": "Königsstuhl",
+        "answer_4": "Steinhuder Meer",
+        "right_answer": 3
+    },
+    {
+        'category': 'rügen',
+        'background': 'img/rügen.jpeg',
+        "question": "Welche Aussage über die Insel ist wahr?",
+        "answer_1": "Die Einwohner nennen sich auch 'Rüganer'",
+        "answer_2": "Rügen ist 63 km breit",
+        "answer_3": "Auf Rügen gibt es außer dem 'Rasenden Roland' keinen weiteren Zugverkehr",
+        "answer_4": "Auf Rügen gibt es nur eine Seebrücke",
+        "right_answer": 1
+    },
+    /* the Middle */
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Die Serie dreht sich um welches Thema?",
+        "answer_1": "Um die Erkundung einer neuen Welt",
+        "answer_2": "Um das Leben der Familie Heck",
+        "answer_3": "Um das Leben in einer Großstadt",
+        "answer_4": "Um eine Zeitreise ins Mittelalter",
+        "right_answer": 2
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Aus wie vielen Mitgliedern besteht die Hauptfamilie?",
+        "answer_1": "Aus Zwei Mitgliedern",
+        "answer_2": "Aus Drei Mitgliedern",
+        "answer_3": "Aus Vier Mitgliedern",
+        "answer_4": "Aus Fünf Mitgliedern",
+        "right_answer": 4
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Aus wie vielen Kindern besteht die Familie?",
+        "answer_1": "Aus zwei Töchtern und einem Sohn",
+        "answer_2": "Aus drei Töchtern",
+        "answer_3": "Aus einer Tochter und zwei Söhnen",
+        "answer_4": "Aus drei Söhnen",
+        "right_answer": 3
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Die Handlung wird aus der sicht von ... erzählt",
+        "answer_1": "dem Vater",
+        "answer_2": "der Tochter",
+        "answer_3": "der Mutter",
+        "answer_4": "dem großen Sohn",
+        "right_answer": 3
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Die Familie lebt in welcher Stadt?",
+        "answer_1": "Orson in Indiana",
+        "answer_2": "Manhattan in Kansas",
+        "answer_3": "Brooklyn in Iowa",
+        "answer_4": "Springfield in Ohio",
+        "right_answer": 1
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Wie viele Staffeln gibt es von 'The Middle'?",
+        "answer_1": "Sechs Staffeln",
+        "answer_2": "Sieben Staffeln",
+        "answer_3": "Acht Staffeln",
+        "answer_4": "Neun Staffeln",
+        "right_answer": 4
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Was macht der Vater der Familie Beruflich?",
+        "answer_1": "Er arbeitet als Verkäufer in einem Autohaus",
+        "answer_2": "Er ist Manager in einem Steinbruch",
+        "answer_3": "Er ist Arbeitslos",
+        "answer_4": "Er wechselt sehr oft seinen Job",
+        "right_answer": 3
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Die Leidenschaft der Mutter ist ...",
+        "answer_1": "eine gute Mutter zu sein",
+        "answer_2": "das Haus täglich zu reinigen",
+        "answer_3": "die Welt zu bereisen",
+        "answer_4": "ihre Wohltätigkeitsarbeit in einem Tierheim",
+        "right_answer": 1
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Welche Aussage über die Tochter 'Sue' stimmt?",
+        "answer_1": "Sie ist die Beliebteste in der Schule",
+        "answer_2": "Sie geht nicht gerne zur Schule",
+        "answer_3": "Sie wird von Lehrern und Mitschülern übersehen",
+        "answer_4": "Sie wird Zuhause unterrichtet",
+        "right_answer": 3
+    },
+    {
+        'category': 'theMiddle',
+        'background': 'img/usa.jpg',
+        "question": "Was für eine Soße stellen die Brüder 'Axl' und 'Brick' her?",
+        "answer_1": "Eine BBQ Soße",
+        "answer_2": "Eine Tomatensoße",
+        "answer_3": "Eine Knoblauchsoße",
+        "answer_4": "Eine Currysoße",
+        "right_answer": 1
+    },
+];
--- script.js
+++ script.js
@@ -1,101 +1,26 @@
-let questions = [{
-        "question": "Leonard und Penny küssen sich nach ihrem ersten Date im Flur: Was bemerkt Leonard an der Decke?",
-        "answer_1": "Eine Überwachungskamera",
-        "answer_2": "Sheldon hat eine Notiz aufgehängt",
-        "answer_3": "Es hängt ein Mistelzweig über der Tür und deswegen küssen sie sich",
-        "answer_4": "Die Lampe ist defekt",
-        "right_answer": 1
-    },
-    {
-        "question": "Wie oft klopft Sheldon an Pennys Tür?",
-        "answer_1": "Drei mal",
-        "answer_2": "Sechs mal",
-        "answer_3": "Neun mal",
-        "answer_4": "Zwölf mal",
-        "right_answer": 3
-    },
-    {
-        "question": "Wo hat Howard vor der Beziehung mit Bernadette gewohnt?",
-        "answer_1": "Auf der Straße",
-        "answer_2": "Bei seiner Mutter",
-        "answer_3": "Bei seinem besten Freund Raj",
-        "answer_4": "In seiner eigenen Wohnung",
-        "right_answer": 2
-    },
-    {
-        "question": "Wer heiratet zuerst?",
-        "answer_1": "Leonard & Penny",
-        "answer_2": "Raj & Bernadette",
-        "answer_3": "Sheldon & Amy",
-        "answer_4": "Howard & Bernadette",
-        "right_answer": 4
-    },
-    {
-        "question": "Was ist Raj's Problem mit Frauen?",
-        "answer_1": "Er steht nicht auf Frauen",
-        "answer_2": "Er kann nicht reden wenn eine Frau im Raum ist, außer unter Alkoholeinfluß",
-        "answer_3": "Er kriegt eine schwache Blase wenn er mit einer Frau redet",
-        "answer_4": "Er lernt nie eine Frau kennen",
-        "right_answer": 2
-    },
-    {
-        "question": "Welches Lied singt Penny Sheldon vor wenn er krank ist?",
-        "answer_1": "Katzentanzlied",
-        "answer_2": "Kommt ein Vogel geflogen",
-        "answer_3": "Hänsel und Gretel",
-        "answer_4": "Old Macdonald Had a Farm",
-        "right_answer": 1
-    },
-    {
-        "question": "Wo im Appartement von Leonard und Sheldon befindet sich eine Kamera?",
-        "answer_1": "In der Deckenlampe",
-        "answer_2": "In der Aquaman Figur",
-        "answer_3": "In der Küche",
-        "answer_4": "Hinter einem Buch",
-        "right_answer": 2
-    },
-    {
-        "question": "In welcher Staffel kommt Amy dazu?",
-        "answer_1": "In der zweiten Staffel",
-        "answer_2": "In der dritten Staffel",
-        "answer_3": "In der vierten Staffel",
-        "answer_4": "In der fünften Staffel",
-        "right_answer": 2
-    },
-    {
-        "question": "Wie nennen die anderen Sheldon und Amy auch?",
-        "answer_1": "Die Besserwisser",
-        "answer_2": "Freaks",
-        "answer_3": "Es gibt keinen Spitznamen",
-        "answer_4": "Shamy",
-        "right_answer": 4
-    },
-    {
-        "question": "Hat Sheldon Geschwister?",
-        "answer_1": "Er hat keine Geschwister",
-        "answer_2": "Einen Zwillingsbruder",
-        "answer_3": "Eine Schwester",
-        "answer_4": "Eine Zwillingsschwester und einen Bruder",
-        "right_answer": 4
-    },
-];
-
+let filterCategory = [];
 let score = 0; /* der punktestand startet bei 0 und wird bei jeder richtigen antwort um 1 erhöht */
 let currentQuestion = 0; /* Der anfangswert, damit er bei dem ersten json array inhalt anfängt */
-let audio_good = new Audio('good.mp3'); /* audio "Richtige Frage" */
-let audio_wrong = new Audio('wrong.mp3'); /* audio "Falsche Frage" */
+let audio_good = new Audio('sounds/good.mp3'); /* audio "Richtige Frage" */
+let audio_wrong = new Audio('sounds/wrong.mp3'); /* audio "Falsche Frage" */
 
-function init() {
+function initQuestion(questionCategory) {
+    changeContainer();
+    let category = questions.filter(questions => questions.category === questionCategory); /* packt alle fragen mit der 'questionCategory' aus dem Parameter in ein neues Array */
+    filterCategory.push(category);
     showAmmountOfQuestions();
     showQuestion();
+    document.getElementById('img').src = filterCategory[0][currentQuestion]['background'];
 }
 
 function showQuestion() {
-    if (gameIsOver()) {
-        showEndScreen();
-    } else {
-        updateProgressBar();
-        showNextQuestion();
+    if (filterCategory.length == 1) {
+        if (gameIsOver()) {
+            showEndScreen();
+        } else {
+            updateProgressBar();
+            showNextQuestion();
+        }
     }
 }
 
@@ -153,8 +78,8 @@ function addAllAnswerClasses() {
 }
 
 function calcPercent() {
-    let percentcomma = (currentQuestion + 1) / questions.length;
-    let percent = percentcomma * 100;
+    let percentcomma = (currentQuestion + 1) / filterCategory[0].length;
+    let percent = Math.round(percentcomma * 100); /* Damit er nur ganze zahlen ausgibt */
     return percent;
 }
 
@@ -175,22 +100,21 @@ function addOnClickAndHover() { /* TODO funktuniert noch nicht, er fügt das onc
 }
 
 function showEndScreen() {
-    /* Show endscreen */
     question_titel.innerHTML = 'Ende!'
     answer_1.innerHTML = '';
     answer_2.innerHTML = 'Du hast es bis zum Ende geschafft!';
-    answer_3.innerHTML = `Du hast <b>${score}</b> von <b>${questions.length}</b> Fragen richtig beantwortet!`;
+    answer_3.innerHTML = `Du hast <b>${score}</b> von <b>${filterCategory[0].length}</b> Fragen richtig beantwortet!`;
     answer_4.innerHTML = '';
 
     removeAllAnswerClasses();
-    document.getElementById('next-button').parentNode.style.display = 'none'; /* löscht den ganzen unteren bereich wo der Button ist */
+    document.getElementById('questionFooter').style.display = 'none'; /* löscht den ganzen unteren bereich wo der Button ist */
     document.getElementById('questionFooter2').style = ''; /* fügt den button "neues spiel" ein */
-    document.getElementById('img').src = "medal.png"; /* ändert das bild im endscreen */
+    document.getElementById('img').src = "img/medal.png"; /* ändert das bild im endscreen */
     document.getElementById('progress-bar').parentNode.classList.add("hide"); /* lässt die progressbar verschwinden */
 }
 
 function gameIsOver() {
-    return currentQuestion >= questions.length;
+    return currentQuestion >= filterCategory[0].length;
 }
 
 function updateProgressBar() {
@@ -199,23 +123,17 @@ function updateProgressBar() {
 }
 
 function showNextQuestion() {
-    let question = questions[currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
+    let question = filterCategory[0][currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
 
-    let question_titel = document.getElementById('question_titel'); /* Für die bessere lesbarkeit der nachfolgenden */
-    let answer_1 = document.getElementById('answer_1');
-    let answer_2 = document.getElementById('answer_2');
-    let answer_3 = document.getElementById('answer_3');
-    let answer_4 = document.getElementById('answer_4');
-
-    question_titel.innerHTML = question["question"]; /* fügt die aktuelle frage ein */
-    answer_1.innerHTML = question["answer_1"];
-    answer_2.innerHTML = question["answer_2"];
-    answer_3.innerHTML = question["answer_3"];
-    answer_4.innerHTML = question["answer_4"];
+    document.getElementById('question_titel').innerHTML = question["question"]; /* fügt die aktuelle frage ein */
+    document.getElementById('answer_1').innerHTML = question["answer_1"];
+    document.getElementById('answer_2').innerHTML = question["answer_2"];
+    document.getElementById('answer_3').innerHTML = question["answer_3"];
+    document.getElementById('answer_4').innerHTML = question["answer_4"];
 }
 
 function pressRightAnswer(answer) {
-    let question = questions[currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
+    let question = filterCategory[0][currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
     let answerNumber = answer.slice(-1); /* Damit nimmt er die letzte zahl, also aus 'answer_3' wird '3' */
 
     return answerNumber == question["right_answer"];
@@ -227,7 +145,7 @@ function updateColorAndScore(answer) {
 }
 
 function showBothColors(answer) {
-    let question = questions[currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
+    let question = filterCategory[0][currentQuestion]; /* für die bessere Lesbarkeit, Damit die nachfolgende schreibweise leichter wird */
     let idOfRightAnswer = `answer_${question["right_answer"]}`; /* Könnte ich auch so reinschreiben, sieht aber so schöner aus */
 
     document.getElementById(answer).parentNode.classList.add('bg-danger');
@@ -248,23 +166,31 @@ function updateCurrentQuestion() {
 }
 
 function notTheEnd() {
-    return currentQuestion < questions.length;
+    return currentQuestion < filterCategory[0].length;
 }
 
 function resetAll() {
+    filterCategory = [];
     currentQuestion = 0; /* setzt den wert wieder auf den anfangswert */
     score = 0; /* setzt den wert wieder auf den anfangswert */
     document.getElementById('questionFooter2').style.display = 'none'; /* der end screen button verschwindet wieder */
-    document.getElementById('img').src = "quiz.jpg"; /* ändert das bild wieder zurück */
+    document.getElementById('img').src = "img/quiz.jpg"; /* ändert das bild wieder zurück */
     document.getElementById('progress-bar').parentNode.classList.remove("hide"); /* zeigt die progress bar wieder an */
-    document.getElementById('next-button').parentNode.style = ''; /* Zeigt den unteren bereich wieder an */
+    document.getElementById('questionFooter').style = ''; /* Zeigt den unteren bereich wieder an */
     addAllAnswerClasses();
     let current_question = document.getElementById('current-question');
     current_question.innerHTML = currentQuestion + 1; /* Damit zeigt er die Aktuelle frage an */
     addOnClickAndHover();
+    document.getElementById('question-container').classList.add('hide'); /* Zeigt den fragebogen an */
+    document.getElementById('category-container').classList.remove('hide'); /* Lässt den kategorie Container verschwinden */
+}
+
+function changeContainer() {
+    document.getElementById('question-container').classList.remove('hide'); /* Zeigt den fragebogen an */
+    document.getElementById('category-container').classList.add('hide'); /* Lässt den kategorie Container verschwinden */
 }
 
 function showAmmountOfQuestions() {
     let question_lenght = document.getElementById('question_lenght');
-    question_lenght.innerHTML = questions.length; /* Zeigt an wie viele Fragen es gibt */
+    question_lenght.innerHTML = filterCategory[0].length; /* Zeigt an wie viele Fragen es gibt */
 }
